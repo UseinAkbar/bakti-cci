c0cfb56c120e004a2ac46a3cf8e38847bd885c9b
Usein <useinakbar01@gmail.com>
2021-12-24 19:56:24 +0700

    configure react-router

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>This is about page</h1>
+            <div id="tentang">Tentang Kita</div>
+        </div>
+    )
+}
+
+export default About
